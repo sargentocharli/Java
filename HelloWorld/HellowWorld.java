1409233972f21a4dc9f8fcd8ff77639ec0b750a6
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2015-12-15 04:00:58 +0100

    Update HellowWorld.java

--- HelloWorld/HellowWorld.java
+++ HelloWorld/HellowWorld.java
@@ -1,8 +1,8 @@
 /**
- * 
+ * The very first program coded by a programmer every time he start with a new language.
  */
-public class HelloWorld{ //
-  public static void main(String[] args){ //
-    System.out.println("Hello World!"); //
-  }
-}
+public class HelloWorld{                  // Declaring class HelloWorld
+  public static void main(String[] args){ // Declaring main class
+    System.out.println("Hello World!");   // Showing on screen the output message: "Hello World"
+  }                                       // End of main class
+}                                         // End of HelloWorld class

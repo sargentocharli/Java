3df70e055cecf4b73ec0926c366562526ec97092
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:34:44 +0100

    Create PosicionNoValidaException.java

---
+++ EstructurasDeDatos/src/listadoEstructurasDeDatosIII/topMusic/PosicionNoValidaException.java
@@ -0,0 +1,15 @@
+package listadoEstructurasDatosIII.topMusic;
+
+public class PosicionNoValidaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public PosicionNoValidaException(String msj){
+		
+		super(msj);
+	}
+
+}

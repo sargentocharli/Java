1137481ab29e9354ce0a3cccb265efba91cbd971
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:03:46 +0100

    Create DecrementoNoValidoException.java

---
+++ EstructurasDeDatos/src/listadoEstructurasDeDatosIII/listaDeLaCompra/DecrementoNoValidoException.java
@@ -0,0 +1,14 @@
+package listadoEstructurasDatosIII.listaDeLaCompra;
+
+public class DecrementoNoValidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	DecrementoNoValidoException(String msj){
+		super(msj);
+	}
+
+}

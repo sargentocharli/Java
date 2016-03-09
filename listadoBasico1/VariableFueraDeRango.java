d0e1492659577db0ed48b2c0654281f8e77bda93
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:54:42 +0100

    Create VariableFueraDeRango.java

---
+++ listadoBasico1/VariableFueraDeRango.java
@@ -0,0 +1,9 @@
+public class VariableFueraDeRango{
+	public static void main (String[] args) {
+		{
+			int i= 1800; byte b= (byte)i;
+			System.out.println("\Valor dentro de rango (int): " +i);
+			System.out.println("Valor fuera de rango (byte): " +b);
+		}
+	}
+}

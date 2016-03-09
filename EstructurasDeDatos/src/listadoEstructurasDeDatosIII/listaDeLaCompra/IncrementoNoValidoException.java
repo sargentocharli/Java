fe2462f55b607c5d6d3bf74c92d7d2946503b9ed
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:04:34 +0100

    Create IncrementoNoValidoException.java

---
+++ EstructurasDeDatos/src/listadoEstructurasDeDatosIII/listaDeLaCompra/IncrementoNoValidoException.java
@@ -0,0 +1,15 @@
+package listadoEstructurasDatosIII.listaDeLaCompra;
+
+public class IncrementoNoValidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	IncrementoNoValidoException(String msj){
+		
+		super(msj);
+	}
+
+}

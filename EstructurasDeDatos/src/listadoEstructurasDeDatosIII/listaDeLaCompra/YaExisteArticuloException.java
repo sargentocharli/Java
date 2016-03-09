bfe73b344d1a0d827dc61c3eb3d2d02b4a152edb
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:07:24 +0100

    Create YaExisteArticuloException.java

---
+++ EstructurasDeDatos/src/listadoEstructurasDeDatosIII/listaDeLaCompra/YaExisteArticuloException.java
@@ -0,0 +1,15 @@
+package listadoEstructurasDatosIII.listaDeLaCompra;
+
+public class YaExisteArticuloException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	YaExisteArticuloException(String msj){
+		
+		super(msj);
+	}
+
+}

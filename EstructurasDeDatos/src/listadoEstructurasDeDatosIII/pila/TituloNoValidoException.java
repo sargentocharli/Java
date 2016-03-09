de28c7e63951067b25ca0791e6c991421d8e2770
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:28:05 +0100

    Create TituloNoValidoException.java

---
+++ EstructurasDeDatos/src/listadoEstructurasDeDatosIII/pila/TituloNoValidoException.java
@@ -0,0 +1,14 @@
+package listadoEstructurasDatosIII.pila;
+
+public class TituloNoValidoException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TituloNoValidoException(String msj){
+	super(msj);
+	}
+
+}

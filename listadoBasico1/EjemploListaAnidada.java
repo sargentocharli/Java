aeb7866a35be108a498841ace69f9188020d145e
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:48:49 +0100

    Create EjemploListaAnidada.java

---
+++ listadoBasico1/EjemploListaAnidada.java
@@ -0,0 +1,24 @@
+/**
+* Crea la clase EjemploListaAnidada que muestre la siguiente salida.
+* Para ello utiliza el método System.out.print() con los caracteres \n y \t
+*
+* @author Lucas Márquez Muñoz
+*/
+
+public class EjemploListaAnidada{
+	
+}
+	{ public static void main (String[] args)
+		{
+		System.out.println("\n \t <ul>"
+		+"\n \t \t <li>"
+		+"\n \t \t <li>"
+		+"\n \t \t <li>"
+		+"\n \t \t \t <ul>"
+		+"\n \t \t \t \t<li>"
+		+"\n \t \t \t \t<li>"
+		+"\n \t \t \t \t<li>"
+		+"\n \t \t \t <\\ul>"
+		+"\n \t <\\ul>");
+		}
+	}

2964cbef97ce0b6a856add9d96341479aa14b97e
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:31:49 +0100

    Create AnnoGrabacionNoValidoException.java

---
+++ EstructurasDeDatos/src/listadoEstructurasDeDatosIII/topMusic/AnnoGrabacionNoValidoException.java
@@ -0,0 +1,14 @@
+package listadoEstructurasDatosIII.topMusic;
+
+public class AnnoGrabacionNoValidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AnnoGrabacionNoValidoException(String msj){
+		
+		super(msj);
+	}
+}

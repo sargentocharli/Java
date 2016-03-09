26410e2fe5786857e1b805d22c9d80c0b6a0c5b8
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:05:23 +0100

    Create ListaVaciaException.java

---
+++ EstructurasDeDatos/src/listadoEstructurasDeDatosIII/listaDeLaCompra/ListaVaciaException.java
@@ -0,0 +1,15 @@
+package listadoEstructurasDatosIII.listaDeLaCompra;
+
+public class ListaVaciaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	ListaVaciaException(String msj){
+		
+		super(msj);
+	}
+
+}

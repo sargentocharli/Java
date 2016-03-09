469a6d4f4e233340b2598313fab789987751c6f1
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 08:35:05 +0100

    Create TransferenciaNoValidaException.java

---
+++ EstructurasDeDatos/src/cuentaCorrienteYTitulares/TransferenciaNoValidaException.java
@@ -0,0 +1,15 @@
+package cuentaCorrienteYTitulares;
+
+public class TransferenciaNoValidaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	TransferenciaNoValidaException(String msj){
+		
+		super(msj);
+	}
+
+}

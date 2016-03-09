8b2f7360e046d0f8cf7a8177a5a6238ebc766bf4
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:47:29 +0100

    Create CaracteresEspagnoles.java

---
+++ listadoBasico1/CaracteresEspagnoles.java
@@ -0,0 +1,16 @@
+/**
+* Crea la clase CaracteresEspagnoles que muestre la siguiente salida.
+* Para ello utiliza el método System.out.print() con los caracteres \n y \t.
+* Utiliza el formato de carácter \udddd
+*
+* @author Lucas Márquez Muñoz
+*/
+
+public class CaracteresEspagnoles
+	{
+	public static void main (String[] args)
+		{
+		System.out.println("Estos caracteres son muy nuestros:");
+		System.out.println("\u00e1 \t \u00ed \t \u00f3 \t \u00fa \t \u00f1 \t \u00d1 \t \u00aa \t \u00ba \n");
+		}
+	}

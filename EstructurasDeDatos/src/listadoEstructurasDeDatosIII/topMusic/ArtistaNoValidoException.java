bfce33aa0a9691b219263c10c685ce8b10099c60
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:32:03 +0100

    Create ArtistaNoValidoException.java

---
+++ EstructurasDeDatos/src/listadoEstructurasDeDatosIII/topMusic/ArtistaNoValidoException.java
@@ -0,0 +1,16 @@
+package listadoEstructurasDatosIII.topMusic;
+
+public class ArtistaNoValidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ArtistaNoValidoException(String msj){
+		
+		super(msj);
+		
+	}
+
+}

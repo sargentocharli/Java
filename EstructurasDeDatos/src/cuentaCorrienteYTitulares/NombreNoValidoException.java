7218aac2447397efb955c80dfcf487f9eb348650
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 08:34:12 +0100

    Create NombreNoValidoException.java

---
+++ EstructurasDeDatos/src/cuentaCorrienteYTitulares/NombreNoValidoException.java
@@ -0,0 +1,14 @@
+package cuentaCorrienteYTitulares;
+
+public class NombreNoValidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	NombreNoValidoException(String msj){
+		super(msj);
+	}
+
+}

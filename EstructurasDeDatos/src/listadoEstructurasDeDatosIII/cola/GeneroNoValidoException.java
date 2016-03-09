e79f0145ab9fd095d9005f8a1c544a070fa91068
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:00:51 +0100

    Create GeneroNoValidoException.java

---
+++ EstructurasDeDatos/src/listadoEstructurasDeDatosIII/cola/GeneroNoValidoException.java
@@ -0,0 +1,14 @@
+package listadoEstructurasDatosIII.cola;
+
+public class GeneroNoValidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public GeneroNoValidoException(String msj){
+		super(msj);
+	}
+
+}

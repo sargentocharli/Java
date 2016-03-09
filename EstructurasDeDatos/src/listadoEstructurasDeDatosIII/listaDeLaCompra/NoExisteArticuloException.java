33192fe80d6328c2a895920a8d4e16c70492113d
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:05:57 +0100

    Create NoExisteArticuloException.java

---
+++ EstructurasDeDatos/src/listadoEstructurasDeDatosIII/listaDeLaCompra/NoExisteArticuloException.java
@@ -0,0 +1,15 @@
+package listadoEstructurasDatosIII.listaDeLaCompra;
+
+public class NoExisteArticuloException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public NoExisteArticuloException(String msj){
+		
+		super(msj);
+	}
+
+}

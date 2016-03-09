1b1e43e56911c00dcf4dab1e00f26c3d8ab4dfcc
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:04:16 +0100

    Create ExistenciasNoValidasException.java

---
+++ EstructurasDeDatos/src/listadoEstructurasDeDatosIII/listaDeLaCompra/ExistenciasNoValidasException.java
@@ -0,0 +1,15 @@
+package listadoEstructurasDatosIII.listaDeLaCompra;
+
+public class ExistenciasNoValidasException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ExistenciasNoValidasException(String msj){
+		
+		super(msj);
+	}
+
+}

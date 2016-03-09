af3ba2a73c4647b013628dc037728fdb5f72dde0
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 08:28:28 +0100

    Create ModeloNoValidoException.java

---
+++ EstructurasDeDatos/src/concesionario/ModeloNoValidoException.java
@@ -0,0 +1,14 @@
+package concesionario;
+
+public class ModeloNoValidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	ModeloNoValidoException(String msj){
+		super(msj);
+	}
+
+}

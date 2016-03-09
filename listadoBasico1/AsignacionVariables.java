441c83ff468b783344dccd27395a273db16f09c6
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:47:06 +0100

    Create AsignacionVariables.java

---
+++ listadoBasico1/AsignacionVariables.java
@@ -0,0 +1,21 @@
+/**	
+* Crea una clase AsignacionVariables con el siguiente código. 
+* Corrígela si es necesario y coméntala en el mismo código fuente, 
+* indicando errores y valores de variables.
+*
+* @author Lucas Márquez Muñoz
+* @version 1.0
+*/
+
+public class AsignacionVariables {
+	public static void main (String[] args){
+		{
+			int i; // Hay que asignar un valor inicial a la variable
+			byte b = (byte)i; /* Si el valor dado a i se sale del rango byte (-128 a 127
+								al forzar que sea tipo byte dará un valor diferente para "b"*/	
+			System.out.println(i);
+			System.out.println(b);
+		}
+	}
+}
+		

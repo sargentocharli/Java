dc5d30df75699137ed6205ca879f46a5953d752f
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 09:05:44 +0100

    Create MinimoNoValidoException.java

---
+++ EstructurasDeDatos/src/listadoEstructurasDeDatosIII/listaDeLaCompra/MinimoNoValidoException.java
@@ -0,0 +1,15 @@
+package listadoEstructurasDatosIII.listaDeLaCompra;
+
+public class MinimoNoValidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public MinimoNoValidoException(String msj){
+		
+		super(msj);
+	}
+
+}

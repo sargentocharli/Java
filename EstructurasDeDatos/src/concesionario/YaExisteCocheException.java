025787a0131433df405c295e94c91844fdda27b0
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-09 08:29:35 +0100

    Create YaExisteCocheException.java

---
+++ EstructurasDeDatos/src/concesionario/YaExisteCocheException.java
@@ -0,0 +1,14 @@
+package concesionario;
+
+public class YaExisteCocheException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	YaExisteCocheException(String msj){
+		super(msj);
+	}
+
+}

4f54bd4cc14c45baf6494892bf7049bbf864f17c
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-17 02:56:07 +0100

    Create FormatoNoValidoException.java

---
+++ EstructurasDeDatos/src/utiles/FormatoNoValidoException.java
@@ -0,0 +1,15 @@
+package utiles;
+
+public class FormatoNoValidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	FormatoNoValidoException(String msj){
+		
+		super(msj);
+	}
+
+}

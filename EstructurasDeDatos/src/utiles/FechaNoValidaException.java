5569d27e37cefa00fc6701fa01cbc40e95f1dac7
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-17 02:56:23 +0100

    Create FechaNoValidaException.java

---
+++ EstructurasDeDatos/src/utiles/FechaNoValidaException.java
@@ -0,0 +1,15 @@
+package utiles;
+
+public class FechaNoValidaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	FechaNoValidaException(String msj){
+		
+		super(msj);
+	}
+
+}

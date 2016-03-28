71757c3d3d9bad505d4d352dd01c39cd28d16627
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-03-28 03:51:08 +0200

    Create SumaNoValidaException.java

---
+++ EstructurasDeDatos/src/utiles/SumaNoValidaException.java
@@ -0,0 +1,15 @@
+package utiles;
+
+public class SumaNoValidaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	SumaNoValidaException(String msj){
+		
+		super(msj);
+	}
+
+}
